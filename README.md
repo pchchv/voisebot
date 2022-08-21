79dd96624dff769fdb62502b76dc5e2d1843799a
pchchv <ipchchv@gmail.com>
2022-08-21 04:09:57 +0300

    Implement bot launch

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# voisebot
-A simple bot on Python. Accepts text, and gives you a robot voice
+# voisebot — A simple bot on Python. Accepts text, and gives you a robot voiceю
+
+## Launch bot
+```commandline
+python bot.py
+```
+###### or
+```commandline
+python3 bot.py
+```
--- bot.py
+++ bot.py
@@ -1,5 +1,20 @@
 import os
+import logging
 from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, executor
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+
+# Initialize bot and dispatcher
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
