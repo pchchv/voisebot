2ba5fb7c705f1f35c6353330748a040ff49620b3
pchchv <ipchchv@gmail.com>
2022-08-21 04:16:33 +0300

    Implement a handler for greeting message

--- bot.py
+++ bot.py
@@ -1,19 +1,26 @@
 import os
 import logging
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher, executor, types
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-
 # Configure logging
 logging.basicConfig(level=logging.INFO)
-
 
 # Initialize bot and dispatcher
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or '/help' command
+    """
+    greeting = ''  # TODO: Write a greeting message
+    await message.reply(greeting)
 
 
 if __name__ == '__main__':
